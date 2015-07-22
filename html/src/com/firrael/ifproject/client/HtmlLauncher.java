4934b6f987fe0c0de565b665db7b8e91b56d7b9c
Railag <whenthegroundcavedin@gmail.com>
2015-07-22 03:12:45 +0300

    libgdx setup

---
+++ html/src/com/firrael/ifproject/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.firrael.ifproject.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.firrael.ifproject.MyGdxGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new MyGdxGame();
+        }
+}
