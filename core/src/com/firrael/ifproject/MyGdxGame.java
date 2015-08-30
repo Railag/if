9cf6d1367f7b05153c62765ec5294d9ca0ab3000
Railag <whenthegroundcavedin@gmail.com>
2015-08-30 15:23:48 +0300

    grid of ships models

--- core/src/com/firrael/ifproject/MyGdxGame.java
+++ core/src/com/firrael/ifproject/MyGdxGame.java
@@ -2,8 +2,11 @@ package com.firrael.ifproject;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.assets.loaders.ModelLoader;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Mesh;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.VertexAttributes;
 import com.badlogic.gdx.graphics.g3d.Environment;
@@ -13,67 +16,90 @@ import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.loader.ObjLoader;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
 
 public class MyGdxGame extends ApplicationAdapter {
 
     public PerspectiveCamera cam;
-    public Model model;
-    public ModelInstance instance;
-    public ModelBatch modelBatch;
-
-    public Environment environment;
-
     public CameraInputController camController;
-
-    private float x = 0f, y = 0f, z = 0f;
+    public ModelBatch modelBatch;
+    public AssetManager assets;
+    public Array<ModelInstance> instances = new Array<ModelInstance>();
+    public Environment environment;
+    public boolean loading;
 
     @Override
     public void create() {
 
+        modelBatch = new ModelBatch();
         environment = new Environment();
         environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
         environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
 
-        modelBatch = new ModelBatch();
-
         cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        cam.position.set(10f, 10f, 10f);
+        cam.position.set(7f, 7f, 7f);
         cam.lookAt(0, 0, 0);
         cam.near = 1f;
         cam.far = 300f;
         cam.update();
 
-        ModelBuilder modelBuilder = new ModelBuilder();
-        model = modelBuilder.createBox(5f, 5f, 5f,
-                new Material(ColorAttribute.createDiffuse(Color.GREEN)),
-                VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
-        instance = new ModelInstance(model, x, y, z);
-
         camController = new CameraInputController(cam);
         Gdx.input.setInputProcessor(camController);
+
+        assets = new AssetManager();
+        assets.load("data/ship.obj", Model.class);
+        loading = true;
+    }
+
+    private void doneLoading() {
+        Model ship = assets.get("data/ship.obj", Model.class);
+        for (float x = -5f; x <= 5f; x += 2f) {
+            for (float z = -5f; z <= 5f; z += 2f) {
+                ModelInstance shipInstance = new ModelInstance(ship);
+                shipInstance.transform.setToTranslation(x, 0, z);
+                instances.add(shipInstance);
+            }
+        }
+        loading = false;
     }
 
     @Override
     public void render() {
+        if (loading && assets.update())
+            doneLoading();
+        camController.update();
+
         Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
-        Vector3 vector = new Vector3();
-        instance.transform.getTranslation(vector);
+//        Vector3 vector = new Vector3();
+//        instance.transform.getTranslation(vector);
+//
+//        vector.x += 0.001f;
+//        vector.y += 0.001;
+//        vector.z += 0.001;
+//
+//
+//        instance.transform.setTranslation(vector);
 
-        vector.x += 0.05f;
-        vector.y += 0.05f;
-        vector.z += 0.05f;
+//        Vector3 camVector = new Vector3();
+//
+//        camVector.x = vector.x + 10f;
+//        camVector.y = vector.y + 10f;
+//        camVector.z = vector.z + 10f;
+//
+//        cam.translate(camVector);
+    //    cam.transform(c);
 
-        instance.transform.setTranslation(vector);
 
         //instance = new ModelInstance(model, vector);
 
         modelBatch.begin(cam);
-        modelBatch.render(instance, environment);
+        modelBatch.render(instances, environment);
         modelBatch.end();
 
         camController.update();
@@ -81,7 +107,8 @@ public class MyGdxGame extends ApplicationAdapter {
 
     @Override
     public void dispose() {
-        model.dispose();
         modelBatch.dispose();
+        instances.clear();
+        assets.dispose();
     }
 }
